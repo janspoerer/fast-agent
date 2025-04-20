5bdcec2ee6f1fc214001c70c19ae79317d0f6671
evalstate <1936278+evalstate@users.noreply.github.com>
2025-04-20 19:30:47 +0100

    vision examples #1

---
+++ examples/mcp/vision-examples/example2.py
@@ -0,0 +1,21 @@
+import asyncio
+from pathlib import Path
+
+from mcp_agent.core.fastagent import FastAgent
+from mcp_agent.core.prompt import Prompt
+
+# Create the application
+fast = FastAgent("fast-agent example")
+
+
+# Define the agent
+@fast.agent(instruction="You are a helpful AI Agent", servers=["filesystem"])
+async def main():
+    # use the --model command line switch or agent arguments to change model
+    async with fast.run() as agent:
+        await agent.default.generate([Prompt.user(Path("instructions.jpg"))])
+        await agent.interactive()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
